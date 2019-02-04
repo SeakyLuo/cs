1631f3196730a665a6cefa1709f75a2083b086ac
SeakyLuo <haitianluo@umail.ucsb.edu>
2019-02-03 23:41:11 -0800

    Modifications

---
+++ threads/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <unistd.h>
+#include "threads.cpp"
+using namespace std;
+
+void *test_func1(void *args){
+    sleep(5);
+    return NULL;
+}
+
+void *test_func2(void *args){
+    cout << "Test\n";
+    return NULL;
+}
+
+void *test_func3(void *args){
+    return NULL;
+}
+
+int main(){
+    pthread_t tid1;
+    pthread_create(&tid1, NULL, test_func1, NULL);
+    pthread_t tid2;
+    pthread_create(&tid2, NULL, test_func2, NULL);
+    pthread_t tid3;
+    pthread_create(&tid3, NULL, test_func3, NULL);
+}
--- threads/threads.cpp
+++ threads/threads.cpp
@@ -1,23 +1,26 @@
-#include <iostream>
 #include <pthread.h>
+#include <signal.h>
 
 using namespace std;
 
-int pthread_create(pthread_t ∗restrict_thread, const pthread_attr_t ∗restrict_attr, \
-                    void ∗(∗start_routine) (void∗), void ∗restrict_arg);
-void pthread_exit(void ∗value_ptr);
-pthread_t pthread_self(void);
-
-int main(int argc, char *argv[]){
-
+int pthread_create(pthread_t *restrict_thread, const pthread_attr_t *restrict_attr, void *(*start_routine) (void*), void *restrict_arg){
+    return 0;
 }
-
-int pthread_create(pthread_t ∗restrict_thread, const pthread_attr_t ∗restrict_attr, void ∗(∗start_routine) (void∗), void ∗restrict_arg){
-
-}
-void pthread_exit(void ∗value_ptr){
+void pthread_exit(void *value_ptr){
 
 }
 pthread_t pthread_self(void){
-
+    return 0;
+}
+static int ptr_mangle(int p){
+    unsigned int ret;
+    asm(" movl %1, %%eax\;\n"
+        " xorl %%gs:0x18, %%eax;"
+        " roll $0x9, %%eax;"
+        " movl %%eax, %0;"
+    : "=r"(ret)
+    : "r"(p)
+    : "%eax"
+    );
+    return ret;
 }
