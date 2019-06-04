c428f752bdf24626298af8c27f5cf9a85a5e6970
SeakyLuo <haitianluo@umail.ucsb.edu>
2019-06-03 21:38:29 -0700

    Save Work

--- 6/codegeneration.cpp
+++ 6/codegeneration.cpp
@@ -5,170 +5,296 @@
 // all functions must have code, many may be left empty.
 
 void CodeGenerator::visitProgramNode(ProgramNode* node) {
-    // WRITEME: Replace with code if necessary
+    std::cout << ".text\n";
+    std::cout << "# Program\n";
+    node->visit_children(this);
 }
 
 void CodeGenerator::visitClassNode(ClassNode* node) {
-    // WRITEME: Replace with code if necessary
+    std::cout << "# Class\n";
+    currentClassName = node->identifier_1->name;
+    currentClassInfo = (*classTable)[currentClassName];
+    node->visit_children(this);
 }
 
 void CodeGenerator::visitMethodNode(MethodNode* node) {
-    // WRITEME: Replace with code if necessary
+    std::cout << "# Method\n";
+    currentMethodName = node->identifier->name;
+    currentMethodInfo = (*currentClassInfo.methods)[currentMethodName];
+    // Corresponds to visitMethodNode()
+    // • Create a label (class name + “_” + method name)
+    // • Push old %ebp
+    // • Set new %ebp (to current %esp)
+    // • Allocate space for local variables
+    //      • Subtract from stack pointer
+    //      • Look into localsSize of MethodInfo
+    // • Save callee-save registers (%ebx, %edi, %esi)
+    std::cout << ".globl " << currentClassName << '_' << currentMethodName << '\n';
+    std::cout << "push %ebp\n";
+    std::cout << "mov $esp, $ebp\n";
+    std::cout << "push $" << currentMethodInfo.localsSize << "\n";
+    std::cout << "sub $" << currentMethodInfo.localsSize << ", $esp\n";
+    std::cout << "\n"; // Save callee-save registers (%ebx, %edi, %esi)
+    node->visit_children(this);
+    std::cout << "\n";
+    std::cout << "mov $ebp, $ebp\n"; // Restore callee-saved registers
+    std::cout << "pop $ebp\n";
+    std::cout << "ret\n";
+    // • Restore callee-saved registers
+    // • Deallocate local vars space by moving %esp to %ebp
+    // • Restore old base pointer by popping old %ebp from the stack
+    // • Return using return address (ret instruction)
 }
 
 void CodeGenerator::visitMethodBodyNode(MethodBodyNode* node) {
-    // WRITEME: Replace with code if necessary
+    std::cout << "# MethodBody\n";
+    // Don’t need to do much; just executes the statements in your function!
+    node->visit_children(this);
 }
 
 void CodeGenerator::visitParameterNode(ParameterNode* node) {
-    // WRITEME: Replace with code if necessary
+    std::cout << "# Parameter\n";
+    node->visit_children(this);
 }
 
 void CodeGenerator::visitDeclarationNode(DeclarationNode* node) {
-    // WRITEME: Replace with code if necessary
+    node->visit_children(this);
+    if (true){
+        // Class Members
+        std::cout << "# Class Members\n";
+        int index = 0;
+        for (auto iter: *(node->identifier_list)){
+            index -=4;
+            std::cout << "sub $4, %esp\n";
+            std::cout << "push " << index << "(%ebp)\n";
+        }
+    }else{
+        // Method Declaration
+        std::cout << "# Method Declaration\n";
+        int index = 0;
+        for (auto iter: *(node->identifier_list)){
+            index -=4;
+            std::cout << "sub $4, %esp\n";
+            std::cout << "push " << index << "(%ebp)\n";
+        }
+    }
+
 }
 
 void CodeGenerator::visitReturnStatementNode(ReturnStatementNode* node) {
-    // WRITEME: Replace with code if necessary
+    // • Execute/visit the expression
+    // • Take result of last expression from top of stack and place
+    // into %eax
+    // • %eax will be used to return values from functions.
+    std::cout << "# Return Statement\n";
+    node->visit_children(this);
+    std::cout << "xchg %esp, %eax\n";
 }
 
 void CodeGenerator::visitAssignmentNode(AssignmentNode* node) {
-    // WRITEME: Replace with code if necessary
+    std::cout << "# Assignment\n";
+    node->visit_children(this);
 }
 
 void CodeGenerator::visitCallNode(CallNode* node) {
-    // WRITEME: Replace with code if necessary
+    std::cout << "# Call\n";
+    node->visit_children(this);
 }
 
 void CodeGenerator::visitIfElseNode(IfElseNode* node) {
-    // WRITEME: Replace with code if necessary
+    std::cout << "# If\n";
+    node->visit_children(this);
+    std::cout << "pop %eax\n";
+    std::cout << "mov $0, %ebx\n";
+    std::cout << "cmp %eax, %ebx\n";
+    int label = nextLabel();
+    std::cout << "je skip_if_" << label << "\n";
+    // if
+    std::cout << " skip_if_" << label << ":\n";
+    // else
+    if (node->statement_list_2){
+        std::cout << "# Else\n";
+    }
 }
 
 void CodeGenerator::visitWhileNode(WhileNode* node) {
-    // WRITEME: Replace with code if necessary
+    std::cout << "# While\n";
+    node->visit_children(this);
+    std::cout << "pop %eax\n";
+    std::cout << "mov $0, %ebx\n";
+    std::cout << "cmp %eax, %ebx\n";
+    int label = nextLabel();
+    std::cout << "je skip_if_" << label << "\n";
+    // execute statements
+    // jump back?
 }
 
 void CodeGenerator::visitPrintNode(PrintNode* node) {
-    // WRITEME: Replace with code if necessary
+    std::cout << "# Print\n";
+    node->visit_children(this);
 }
 
 void CodeGenerator::visitDoWhileNode(DoWhileNode* node) {
-    // WRITEME: Replace with code if necessary
+    std::cout << "# DoWhile\n";
+    node->visit_children(this);
 }
 
 void CodeGenerator::visitPlusNode(PlusNode* node) {
+    std::cout << "# Plus\n";
     node->visit_children(this);
-    std::cout << "pop  %ebx" << std::endl;
-    std::cout << "pop  %eax" << std::endl;
-    std::cout << "add  %ebx, %eax" << std::endl;
-    std::cout << "push %eax" << std::endl;
+    std::cout << "pop  %ebx\n";
+    std::cout << "pop  %eax\n";
+    std::cout << "add  %ebx, %eax\n";
+    std::cout << "push %eax\n";
 }
 
 void CodeGenerator::visitMinusNode(MinusNode* node) {
+    std::cout << "# Minus\n";
     node->visit_children(this);
-    std::cout << "pop  %ebx" << std::endl;
-    std::cout << "pop  %eax" << std::endl;
-    std::cout << "sub  %ebx, %eax" << std::endl;
-    std::cout << "push %eax" << std::endl;
+    std::cout << "pop  %ebx\n";
+    std::cout << "pop  %eax\n";
+    std::cout << "sub  %ebx, %eax\n";
+    std::cout << "push %eax\n";
 }
 
 void CodeGenerator::visitTimesNode(TimesNode* node) {
+    std::cout << "# Times\n";
     node->visit_children(this);
-    std::cout << "pop  %ebx" << std::endl;
-    std::cout << "pop  %eax" << std::endl;
-    std::cout << "imul  %ebx, %eax" << std::endl;
-    std::cout << "push %eax" << std::endl;
+    std::cout << "pop  %ebx\n";
+    std::cout << "pop  %eax\n";
+    std::cout << "imul  %ebx, %eax\n";
+    std::cout << "push %eax\n";
 }
 
 void CodeGenerator::visitDivideNode(DivideNode* node) {
+    std::cout << "# Divide\n";
     node->visit_children(this);
-    int sign = node->expression_1->integer->value > 0;
-    std::cout << "pop  %eax" << std::endl;
-    std::cout << "pop  %ebx" << std::endl;
-    std::cout << "mv $" << sign << ", %edx" << std::endl;
-    std::cout << "cdq" << std::endl;
-    std::cout << "idiv  %ebx" << std::endl;
-    std::cout << "push %ebx" << std::endl;
+    // int sign = node->expression_1->integer->value > 0;
+    int sign;
+    std::cout << "pop  %eax\n";
+    std::cout << "pop  %ebx\n";
+    std::cout << "mov $" << sign << ", %edx\n";
+    std::cout << "cdq\n";
+    std::cout << "idiv  %ebx\n";
+    std::cout << "push %ebx\n";
 }
 
 void CodeGenerator::visitGreaterNode(GreaterNode* node) {
+    std::cout << "# Greater\n";
     node->visit_children(this);
-    std::cout << "mv $0, %edx" << std::endl;
-    std::cout << "cmp %ebx, %eax" << std::endl;
-    std::cout << "setg %dl" << std::endl;
-    std::cout << "push %edx" << std::endl;
+    std::cout << "mov $0, %edx\n";
+    std::cout << "cmp %ebx, %eax\n";
+    std::cout << "setg %dl\n";
+    std::cout << "push %edx\n";
 }
 
 void CodeGenerator::visitGreaterEqualNode(GreaterEqualNode* node) {
+    std::cout << "# GreaterEqual\n";
     node->visit_children(this);
-    std::cout << "mv $0, %edx" << std::endl;
-    std::cout << "cmp %ebx, %eax" << std::endl;
-    std::cout << "setge %dl" << std::endl;
-    std::cout << "push %edx" << std::endl;
+    std::cout << "mov $0, %edx\n";
+    std::cout << "cmp %ebx, %eax\n";
+    std::cout << "setge %dl\n";
+    std::cout << "push %edx\n";
 }
 
 void CodeGenerator::visitEqualNode(EqualNode* node) {
+    std::cout << "# Equal\n";
     node->visit_children(this);
-    std::cout << "mv $0, %edx" << std::endl;
-    std::cout << "cmp %ebx, %eax" << std::endl;
-    std::cout << "sete %dl" << std::endl;
-    std::cout << "push %edx" << std::endl;
+    std::cout << "mov $0, %edx\n";
+    std::cout << "cmp %ebx, %eax\n";
+    std::cout << "sete %dl\n";
+    std::cout << "push %edx\n";
 }
 
 void CodeGenerator::visitAndNode(AndNode* node) {
+    std::cout << "# And\n";
     node->visit_children(this);
-    std::cout << "pop  %ebx" << std::endl;
-    std::cout << "pop  %eax" << std::endl;
-    std::cout << "and  %ebx, %eax" << std::endl;
-    std::cout << "push %eax" << std::endl;
+    std::cout << "pop  %ebx\n";
+    std::cout << "pop  %eax\n";
+    std::cout << "and  %ebx, %eax\n";
+    std::cout << "push %eax\n";
 }
 
 void CodeGenerator::visitOrNode(OrNode* node) {
+    std::cout << "# Or\n";
     node->visit_children(this);
-    std::cout << "pop  %ebx" << std::endl;
-    std::cout << "pop  %eax" << std::endl;
-    std::cout << "or   %ebx, %eax" << std::endl;
-    std::cout << "push %eax" << std::endl;
+    std::cout << "pop  %ebx\n";
+    std::cout << "pop  %eax\n";
+    std::cout << "or   %ebx, %eax\n";
+    std::cout << "push %eax\n";
 }
 
 void CodeGenerator::visitNotNode(NotNode* node) {
+    std::cout << "# Not\n";
     node->visit_children(this);
-    std::cout << "pop  %ebx" << std::endl;
-    std::cout << "pop  %eax" << std::endl;
-    std::cout << "not  %ebx, %eax" << std::endl;
-    std::cout << "push %eax" << std::endl;
+    std::cout << "pop  %ebx\n";
+    std::cout << "pop  %eax\n";
+    std::cout << "not  %ebx, %eax\n";
+    std::cout << "push %eax\n";
 }
 
 void CodeGenerator::visitNegationNode(NegationNode* node) {
+    std::cout << "# Negation\n";
     node->visit_children(this);
-    std::cout << "pop  %ebx" << std::endl;
-    std::cout << "pop  %eax" << std::endl;
-    std::cout << "neg  %ebx, %eax" << std::endl;
-    std::cout << "push %eax" << std::endl;
+    std::cout << "pop  %ebx\n";
+    std::cout << "pop  %eax\n";
+    std::cout << "neg  %ebx, %eax\n";
+    std::cout << "push %eax\n";
 }
 
 void CodeGenerator::visitMethodCallNode(MethodCallNode* node) {
-    // WRITEME: Replace with code if necessary
+    // Corresponds to visitMethodCallNode()
+    // • Save caller-save registers (%eax, %ecx, %edx)
+    // • Push arguments on the stack (follow __cdecl style)
+    // • Push self-pointer (we’ll come back to this later)
+    // • Push return address (implicit if using call)
+    // Call the method
+    // • call instruction moves into method’s prologue
+    std::cout << "# MethodCall\n";
+    std::cout << "push %eax;\npush %ecx;\npush %edx;\n";
+    for (auto iter: *(node->expression_list)){
+        std::cout << "push " << iter << "\n";
+    }
+    std::cout << "ret\n";
+    node->visit_children(this);
+    // • Pop return address (done implicitly by ret)
+    // • Remove the arguments
+    // • Retrieve the return value from %eax
+    // • Restore caller-save registers
+    std::cout << "ret\n";
+    for (auto iter: *(node->expression_list)){
+        std::cout << "pop " << iter << "\n";
+    }
+    std::cout << "pop %eax;\npop %ecx;\npop %edx;\n";
+    std::cout << "\n";
 }
 
 void CodeGenerator::visitMemberAccessNode(MemberAccessNode* node) {
-    // WRITEME: Replace with code if necessary
+    node->visit_children(this);
 }
 
 void CodeGenerator::visitVariableNode(VariableNode* node) {
-    // WRITEME: Replace with code if necessary
+    node->visit_children(this);
 }
 
 void CodeGenerator::visitIntegerLiteralNode(IntegerLiteralNode* node) {
-    // WRITEME: Replace with code if necessary
+    node->visit_children(this);
+    std::cout << "push $" << node->integer->value << "\n";
 }
 
 void CodeGenerator::visitBooleanLiteralNode(BooleanLiteralNode* node) {
-    // WRITEME: Replace with code if necessary
+    node->visit_children(this);
+    std::cout << "push $" << node->integer->value << "\n";
 }
 
 void CodeGenerator::visitNewNode(NewNode* node) {
-    // WRITEME: Replace with code if necessary
+    std::cout << "# New\n";
+    node->visit_children(this);
+    // size to allocate
+    std::cout << "push $" << (*classTable)[node->identifier->name].membersSize << "\n";
+    std::cout << "call malloc\n"; // call malloc with one arg
+    std::cout << "add $4, %esp\n"; // remove arg from stack
+    std::cout << "push %eax\n"; // push returned pointer
 }
 
 void CodeGenerator::visitIntegerTypeNode(IntegerTypeNode* node) {
@@ -180,7 +306,7 @@ void CodeGenerator::visitBooleanTypeNode(BooleanTypeNode* node) {
 }
 
 void CodeGenerator::visitObjectTypeNode(ObjectTypeNode* node) {
-    // WRITEME: Replace with code if necessary
+    node->visit_children(this);
 }
 
 void CodeGenerator::visitNoneNode(NoneNode* node) {
